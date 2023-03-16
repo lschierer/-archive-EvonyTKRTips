df5e99c88322859938f21ff94fd289b53499c231
Luke Schierer <lschiere@amazon.com>
2023-03-16 16:33:25 -0400

    configure a couple of variables, get rid of twitter and open graph

--- astrodocs/src/consts.ts
+++ astrodocs/src/consts.ts
@@ -1,18 +1,8 @@
 export const SITE = {
-	title: 'Documentation',
-	description: 'Your website description.',
+	title: 'Evony TKR Tips',
+	description: 'Tips for Budget Gamers',
 	defaultLanguage: 'en-us',
 } as const;
-
-export const OPEN_GRAPH = {
-	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
-	},
-	twitter: 'astrodotbuild',
-};
 
 export const KNOWN_LANGUAGES = {
 	English: 'en',
