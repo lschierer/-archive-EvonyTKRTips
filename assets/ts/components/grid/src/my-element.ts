365c60e7992f11a0d74a305a8957d20d5752126d
Luke Schierer <luke@schierer.org>
2023-03-16 01:48:32 -0400

    partially through https://www.htmlelements.com/docs/lit-element/

--- assets/ts/components/grid/src/my-element.ts
+++ assets/ts/components/grid/src/my-element.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google LLC
+ * Luke Schierer
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -8,14 +8,14 @@ import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 /**
- * An example element.
+ * Parent element for Smart Web Components Grid object
  *
  * @fires count-changed - Indicates when the count changes
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('grid')
+export class Grid extends LitElement {
   static override styles = css`
     :host {
       display: block;
@@ -63,6 +63,6 @@ export class MyElement extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement;
+    'grid': Grid;
   }
 }
