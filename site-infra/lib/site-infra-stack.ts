b98a9c9c541bb70cd468ce413a345a9111b7b09e
Luke Stephen Schierer <luke@schierer.org>
2023-03-27 14:01:54 -0400

    start using cdk to manage

---
+++ site-infra/lib/site-infra-stack.ts
@@ -0,0 +1,38 @@
+import { Construct } from 'constructs';
+import { Stack, StackProps, NestedStack, NestedStackProps } from 'aws-cdk-lib';
+import { aws_codebuild as codebuild } from 'aws-cdk-lib';
+import { aws_codecommit as codecommit } from 'aws-cdk-lib';
+
+import * as amplify from '@aws-cdk/aws-amplify-alpha'
+
+export class SiteInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Part 1 [Optional] - Creation of the source control repository
+    const EvonyTKRTipsRepo = new codecommit.Repository(
+      this,
+      "EvonyTKRTipsRepo",
+      {
+        repositoryName: "EvonyTKRTips",
+        description:
+          "Code for both the infrastructure and site for EvonyTKRTips",
+      }
+    );
+
+		const amplifyApp = new amplify.App(this, "EvonyTKRTips", {
+			sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+				repository: EvonyTKRTipsRepo,
+			}),
+		});
+		const masterBranch = amplifyApp.addBranch("master");
+
+		const domain = amplifyApp.addDomain('evonytkrtips.net', {
+			enableAutoSubdomain: true,
+			autoSubdomainCreationPatterns: ['*', 'pr*'],
+		});
+		domain.mapRoot(masterBranch);
+		domain.mapSubDomain(masterBranch, 'www');
+
+  }
+}
