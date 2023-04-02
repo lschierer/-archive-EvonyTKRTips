e5fbebe581be2a5b1484d3eb29636fecfd57c9ef
Luke Schierer <luke@schierer.org>
2023-04-02 01:48:12 -0400

    move hugo into vite

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,6 +1,19 @@
-import { defineConfig } from 'vite'
-import { resolve,join } from 'path';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import hugoPlugin from 'vite-hugo-plugin'
+
+console.log(typeof(hugoPlugin));
+
+// Root directory of our application
+const appDir = __dirname;
+
+// The directory where hugo builds it's files.
+const hugoOutDir = resolve(appDir, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-})
+    plugins: [
+      hugoPlugin({ appDir, hugoOutDir })
+    ], 
+});
+
